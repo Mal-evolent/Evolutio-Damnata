e8414445bbdf333f3c5f247ca40ee6c9c18a3fbb
M-A-OFFICIAL <44640349+MichaelAjayi01@users.noreply.github.com>
2024-11-14 13:25:28 +0000

    Deck class created

---
+++ prototype/Damnatio/Assets/Scripts/Deck.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deck : MonoBehaviour
+{
+    public List<Card> Cards = new List<Card>();
+    public List<Card> Hand = new List<Card>();
+    public int MaxDeckSize = 30;
+    public int HandSize = 5;
+
+    public CardLibrary cardLibrary;
+
+    void Start()
+    {
+        Debug.Log("game started");
+
+        if (cardLibrary != null)
+        {
+            PopulateDeck();
+        }
+        else
+        {
+            Debug.LogWarning("Card Library not assigned to Deck. Please assign a Card Library.");
+        }
+    }
+
+    public void PopulateDeck()
+    {
+        List<Card> newDeck = cardLibrary.CreateDeckFromLibrary();
+        foreach (Card card in newDeck)
+        {
+            AddCard(card);
+        }
+
+        Debug.Log("Deck Populated");
+    }
+
+    public void Shuffle()
+    {
+        for (int i = 0; i < Cards.Count; i++)
+        {
+            Card temp = Cards[i];
+            int randomIndex = Random.Range(i, Cards.Count);
+            Cards[i] = Cards[randomIndex];
+            Cards[randomIndex] = temp;
+        }
+        Debug.Log("Deck Shuffled");
+    }
+
+    public void DrawCard()
+    {
+        // Ensure the hand is filled up to the hand size limit
+        while (Hand.Count < HandSize && Cards.Count > 0)
+        {
+            Card drawnCard = Cards[0];
+            Cards.RemoveAt(0);
+            Hand.Add(drawnCard);
+            Debug.Log("Drew Card: " + drawnCard.CardName);
+        }
+
+        if (Hand.Count >= HandSize)
+        {
+            Debug.Log("Hand is full. Cannot draw more cards.");
+        }
+    }
+
+    public void AddCard(Card card)
+    {
+        if (Cards.Count >= MaxDeckSize)
+        {
+            Debug.Log("Deck is full");
+            return;
+        }
+
+        Cards.Add(card);
+        Debug.Log("Added Card: " + card.CardName);
+    }
+
+    public void RemoveCard(Card card)
+    {
+        if (Cards.Remove(card))
+        {
+            Debug.Log("Removed Card: " + card.CardName);
+        }
+        else
+        {
+            Debug.Log("Card not found in deck");
+        }
+    }
+
+    public void Reset()
+    {
+        Cards.Clear();
+        Hand.Clear();
+        Debug.Log("Deck Reset");
+    }
+}
