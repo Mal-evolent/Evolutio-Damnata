8a1d029ad28c32ca92f0de5fd41808acf50e4e27
Mal-evolent <44640349+Mal-evolent@users.noreply.github.com>
2025-04-16 02:28:09 +0100

    new spell effect + new cards

--- prototype/Evolutio Damnata/Assets/Scripts/Key Words/SpellEffect.cs
+++ prototype/Evolutio Damnata/Assets/Scripts/Key Words/SpellEffect.cs
@@ -1,11 +1,9 @@
-/*
- * This script is used to define the different types of effects that a spell can have.
- */
 
 public enum SpellEffect
 {
     None,
     Damage,
     Heal,
-    Burn
+    Burn,
+    Draw
 }
